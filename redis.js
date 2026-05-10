1fd3d48a62f2b121bdfcc515280a948ca3bc829f
Shubham Grover <shubhamgrover.data@gmail.com>
2026-05-11 00:49:38 +0530

    err symbol redis.js

--- redis.js
+++ redis.js
@@ -1,17 +1,17 @@
 const Redis = require("ioredis");
 
-const redisClient = new Redis(process.env.REDISURL,{
-    // Add these for better serverless debugging
-    connectTimeout: 10000, // Wait 10s before failing
-    maxRetriesPerRequest: 1, // Don't let it loop forever in a serverless function
+const redisClient = new Redis(process.env.REDISURL, {
+  // Add these for better serverless debugging
+  connectTimeout: 10000, // Wait 10s before failing
+  maxRetriesPerRequest: 1, // Don't let it loop forever in a serverless function
 });
 
 redisClient.on("connect", () => {
   console.log("Redis connected");
 });
 
-redisClient.on("error", () => {
+redisClient.on("error", (err) => {
   console.error("❌ Redis Error:", err.message);
-    //console.error("Full Error Stack:", err.stack);
+  //console.error("Full Error Stack:", err.stack);
 });
 module.exports = { redisClient };
